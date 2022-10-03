e24d5289613c489b872ca41fe572b076c888913b
vrohith24 <70521157+vrohith24@users.noreply.github.com>
2022-10-03 16:18:12 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,10 +1,13 @@
 #!/usr/bin/env groovy
 
-
-
 def call() {
-    def cmd = [ 'bash', '-c', "ls -l".toString()]
-    def result = cmd.execute().text
-
-    return result
+    
+    def service_list = ['bash','-c',"ls -l | sed 's|Services/||g'".toString()]
+    
+    def result = service_list.execute().text
+    def slurper = new JsonSlurper()
+    def json = slurper.parseText(result)
+    def tags = new ArrayList()
+    tags.addAll(json.tags)
+    return tags.join('\n') 
 }
