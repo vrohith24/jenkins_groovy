de10f94f21a286e951fb090e9248ebb1d47483ce
vrohith24 <70521157+vrohith24@users.noreply.github.com>
2022-10-03 16:19:26 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,13 +1,5 @@
 #!/usr/bin/env groovy
 
-def call() {
-    
-    def service_list = ['bash','-c',"ls -l | sed 's|Services/||g'".toString()]
-    
-    def result = service_list.execute().text
-    def slurper = new JsonSlurper()
-    def json = slurper.parseText(result)
-    def tags = new ArrayList()
-    tags.addAll(json.tags)
-    return tags.join('\n') 
+def call(String name = 'User') {
+		echo "Welcome, ${name}."
 }
