40f8bb80c391eedf2d554df05ff8dc87e7f966ad
vrohith24 <70521157+vrohith24@users.noreply.github.com>
2022-10-03 20:24:27 +0530

    Update sample_file.groovy

--- vars/sample_file.groovy
+++ vars/sample_file.groovy
@@ -2,13 +2,5 @@
 import groovy.json.JsonSlurper
 
 def call() {
-    
-    def service_list = ['bash','-c',"ls Services/ | sed 's|Services/||g'".toString()]
-    
-    def result = service_list.execute().text
-    def slurper = new JsonSlurper()
-    def json = slurper.parseText(result)
-    def tags = new ArrayList()
-    tags.addAll(json.tags)
-    return tags.join('\n') 
+    echo "$pwd"
 }
